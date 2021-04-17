f63b39e05619d97fdce957a3c71e8a8e7783c3c2
Yağız Özgün <61112251+yagizozgun@users.noreply.github.com>
2021-04-17 17:46:40 +0300

    .

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> plakalar = new MyDictionary<int, string>();
+            plakalar.Add(01, "Adana");
+            plakalar.Add(02, "Adıyaman");
+            plakalar.Add(03, "Afyonkarahisar");
+            plakalar.Add(04, "Ağrı");
+            plakalar.Add(05, "Aksaray");
+
+            foreach (var item in plakalar.Keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in plakalar.Values)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(plakalar.Count);
+        }
+    }
+}
